b31ce9cb3b7c5fd63a9a8d0c089b65b2286860ac
NerbzHub <nathan.nette@gmail.com>
2017-08-16 11:49:23 +1000

    After Matt Explained how to do it
    This has comments on how to actually implement everything

--- Level Editor/Level Editor/Form1.cs
+++ Level Editor/Level Editor/Form1.cs
@@ -26,6 +26,24 @@ namespace Level_Editor
             texture.Size = new Size(65, 65);
             texture.BackgroundImage = Properties.Resources.tex_Bloons_terrain_grass;
             Controls.Add(texture);
+            for (int i = 0; i < 0; ++i)
+            {
+                tileTexture.Add(texture);
+                texture.Location{Location};
+            }
+            tileTexture.Add(texture);
         }
     }
 }
+
+//https://www.youtube.com/watch?v=lk0GsyxCj-U&index=1&list=PL0A865073DA96A7DA - this is the tutorial in xna
+//Bitmap Blitting
+// Look at picture that matt drew.
+// Create an array of enums. The enums then get assigned to textures. 
+//[1, 1, 2
+// 0, 2, 0
+// 1, 1, 2]
+// The enums will be allocated to different textures which allows the array to be exported and re-imported to retain the same value.
+// This means that the load function will be able to work.
+// Utilize bitmap blitting to allocate where inside a picture box the textures will be placed.
+// The diagram matt drew shows how this will be implemented.
